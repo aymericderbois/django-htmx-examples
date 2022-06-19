7060c394ec797fa5f9ee9d09b4f62dd810d85946
Aymeric Derbois <aymeric@kenlodin.com>
2022-06-19 13:05:21 +0200

    feat(model): add model for todo

--- todomvc/core/models.py
+++ todomvc/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    is_done = models.BooleanField(default=False)
+    session_uuid = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
