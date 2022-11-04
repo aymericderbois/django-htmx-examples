515623248046ff2041c0f746ec8a17f0cea3c359
Aymeric Derbois <azmimik@gmail.com>
2022-11-04 15:12:31 +0100

    fix: urls file for class based view

--- todomvc/cbv_hx_location/urls.py
+++ todomvc/cbv_hx_location/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 
-from todomvc.core.views.cbv.cbv import (
+from todomvc.cbv_hx_location.views import (
     TodoListView,
     TodoActiveListView,
     TodoCompletedListView,
@@ -16,16 +16,54 @@ from todomvc.core.views.cbv.cbv import (
 app_name = "todo"
 
 urlpatterns = [
-    path("list", TodoListView.as_view(), name="list-all"),
-    path("list/active", TodoActiveListView.as_view(), name="list-active"),
-    path("list/completed", TodoCompletedListView.as_view(), name="list-completed"),
-    path("toggle-all", TodoToggleAllView.as_view(), name="toggle-all"),
-    path("partial/item/<int:pk>", TodoItemPartialView.as_view(), name="item-partial"),
     path(
-        "partial/item/<int:pk>/edit", TodoUpdateView.as_view(), name="item-edit-partial"
+        "list",
+        TodoListView.as_view(),
+        name="list-all",
     ),
-    path("clear-completed", TodoClearCompletedView.as_view(), name="clear-completed"),
-    path("cvb/<int:pk>/toggle", TodoToggleView.as_view(), name="toggle"),
-    path("cvb/<int:pk>/delete", TodoDeleteView.as_view(), name="delete-view"),
-    path("cvb/create", TodoCreateView.as_view(), name="create"),
+    path(
+        "list/active",
+        TodoActiveListView.as_view(),
+        name="list-active",
+    ),
+    path(
+        "list/completed",
+        TodoCompletedListView.as_view(),
+        name="list-completed",
+    ),
+    path(
+        "toggle-all",
+        TodoToggleAllView.as_view(),
+        name="toggle-all",
+    ),
+    path(
+        "partial/item/<int:pk>",
+        TodoItemPartialView.as_view(),
+        name="item-partial",
+    ),
+    path(
+        "partial/item/<int:pk>/edit",
+        TodoUpdateView.as_view(),
+        name="item-edit-partial",
+    ),
+    path(
+        "clear-completed",
+        TodoClearCompletedView.as_view(),
+        name="clear-completed",
+    ),
+    path(
+        "cvb/<int:pk>/toggle",
+        TodoToggleView.as_view(),
+        name="toggle",
+    ),
+    path(
+        "cvb/<int:pk>/delete",
+        TodoDeleteView.as_view(),
+        name="delete-view",
+    ),
+    path(
+        "cvb/create",
+        TodoCreateView.as_view(),
+        name="create",
+    ),
 ]
